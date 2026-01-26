9abf43f08fdb2b488fa22fce6e6322cf4b156f63
後藤 真弘 <>
2026-01-27 00:40:16 +0900

    LFMのサイズ変更

--- Book Management System/src/LendingScreen/LFM.java
+++ Book Management System/src/LendingScreen/LFM.java
@@ -14,24 +14,27 @@ public class LFM extends JFrame{
         suf.setLayout(null);
 
 //      ラベル欄
-        JLabel lan ,lab , lalp ,aw , ren , ret;
+        JLabel lan ,lab , lalp ,lapc ,aw , laren , laret;
         lan = new JLabel("氏名");
         lan.setBounds(30, 30, 80, 30);
         suf.add(lan);
+        lapc =new JLabel("郵便番号");
+        lapc.setBounds(30,75,80,30);
+        suf.add(lapc);
         lab = new JLabel("書籍名");
-        lab.setBounds(30, 75, 80, 30);
+        lab.setBounds(30, 110, 80, 30);
         suf.add(lab);
-        ren = new JLabel("貸出日");
-        ren.setBounds(122,110,80,30);
-        suf.add(ren);
-        ret = new JLabel("返却日");
-        ret.setBounds(272,110,80,30);
-        suf.add(ret);
+        laren = new JLabel("貸出日");
+        laren.setBounds(122,135,80,30);
+        suf.add(laren);
+        laret = new JLabel("返却日");
+        laret.setBounds(272,135,80,30);
+        suf.add(laret);
         aw = new JLabel("〜");
-        aw.setBounds(210,135,80,30);
+        aw.setBounds(210,150,80,30);
         suf.add(aw);
         lalp = new JLabel("貸出期間");
-        lalp.setBounds(30,135,80,30);
+        lalp.setBounds(30,160,80,30);
         suf.add(lalp);
 
 //      テキストフォーマット
@@ -40,33 +43,33 @@ public class LFM extends JFrame{
         tfn.setBounds(90,30,105,30);
         suf.add(tfn);
         tfLs = new JTextField();
-        tfLs.setBounds(90,135,105,30);
+        tfLs.setBounds(90,160,105,30);
         suf.add(tfLs);
         tfLe = new JTextField();
-        tfLe.setBounds(240,135,105,30);
+        tfLe.setBounds(240,160,105,30);
         suf.add(tfLe);
 
 //      コンボボックス欄
         String[] bl = {"３ステップでしっかり学ぶ PHP入門" , "３ステップでしっかり学ぶ javaScript入門"
         ,"スラスラわかるHTML&CSSのきほん" ,"ぜったいできます！ ワード＆エクセル超入門",  "ビジネス実務法務試験3級"};
         JComboBox<String> cbl = new JComboBox<String>(bl);
-        cbl.setBounds(90,75,320,30);
+        cbl.setBounds(90,110,320,30);
         suf.add(cbl);
 
 //      ボタン欄
         JButton bur , bub;
         bur = new JButton("貸出");
-        bur.setBounds(240,180,75,35);
+        bur.setBounds(240,320,75,35);
         suf.add(bur);
         bub = new JButton(new BackHomeAction(this));
-        bub.setBounds(325,180,75,35);
+        bub.setBounds(325,320,75,35);
         suf.add(bub);
 
         Container ContentPane = getContentPane();
         ContentPane.add(suf);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(430 ,270);
+        setSize(430 ,400);
         setVisible(true);
     }
 }
