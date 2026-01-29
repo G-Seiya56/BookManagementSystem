d98bca4e1644a52112df9fe3156c3c38c75a8cdc
後藤星也 <>
2026-01-29 23:52:35 +0900

    返却用のフォーマット完成

--- Book Management System/src/LendingScreen/LFM.java
+++ Book Management System/src/LendingScreen/LFM.java
@@ -14,62 +14,74 @@ public class LFM extends JFrame{
         suf.setLayout(null);
 
 //      ラベル欄
-        JLabel lan ,lab , lalp ,lapc ,aw , laren , laret;
+        JLabel lan ,lab , lalp ,lapc ,laad ,aw , laren , laret;
         lan = new JLabel("氏名");
         lan.setBounds(30, 30, 80, 30);
         suf.add(lan);
         lapc =new JLabel("郵便番号");
         lapc.setBounds(30,75,80,30);
         suf.add(lapc);
+        laad = new JLabel("住所");
+        laad.setBounds(30,120,80,30);
+        suf.add(laad);
         lab = new JLabel("書籍名");
-        lab.setBounds(30, 110, 80, 30);
+        lab.setBounds(30, 165, 80, 30);
         suf.add(lab);
         laren = new JLabel("貸出日");
-        laren.setBounds(122,135,80,30);
+        laren.setBounds(122,195,80,30);
         suf.add(laren);
         laret = new JLabel("返却日");
-        laret.setBounds(272,135,80,30);
+        laret.setBounds(272,195,80,30);
         suf.add(laret);
         aw = new JLabel("〜");
-        aw.setBounds(210,150,80,30);
+        aw.setBounds(210,195,80,30);
         suf.add(aw);
         lalp = new JLabel("貸出期間");
-        lalp.setBounds(30,160,80,30);
+        lalp.setBounds(30,220,80,30);
         suf.add(lalp);
 
 //      テキストフォーマット
-        JTextField tfn , tfLs , tfLe;
+        JTextField tfn , tfpc ,tfad , tfLs , tfLe;
         tfn =new JTextField(7);
         tfn.setBounds(90,30,105,30);
         suf.add(tfn);
+        tfpc = new JTextField(8);
+        tfpc.setBounds(90,75, 80,30);
+        suf.add(tfpc);
+        tfad = new JTextField();
+        tfad.setBounds(90,120,320,30);
+        suf.add(tfad);
         tfLs = new JTextField();
-        tfLs.setBounds(90,160,105,30);
+        tfLs.setBounds(90,220,105,30);
         suf.add(tfLs);
         tfLe = new JTextField();
-        tfLe.setBounds(240,160,105,30);
+        tfLe.setBounds(240,220,105,30);
         suf.add(tfLe);
 
 //      コンボボックス欄
         String[] bl = {"３ステップでしっかり学ぶ PHP入門" , "３ステップでしっかり学ぶ javaScript入門"
         ,"スラスラわかるHTML&CSSのきほん" ,"ぜったいできます！ ワード＆エクセル超入門",  "ビジネス実務法務試験3級"};
         JComboBox<String> cbl = new JComboBox<String>(bl);
-        cbl.setBounds(90,110,320,30);
+        cbl.setBounds(90,165,320,30);
         suf.add(cbl);
 
 //      ボタン欄
-        JButton bur , bub;
+        JButton buse, bur , bub;
+        buse = new JButton( "検索");
+        buse.setBounds(175,75,80,30);
+        suf.add(buse);
         bur = new JButton("貸出");
-        bur.setBounds(240,320,75,35);
+        bur.setBounds(240,265,75,35);
         suf.add(bur);
         bub = new JButton(new BackHomeAction(this));
-        bub.setBounds(325,320,75,35);
+        bub.setBounds(325,265,75,35);
         suf.add(bub);
 
         Container ContentPane = getContentPane();
         ContentPane.add(suf);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(430 ,400);
+        setSize(430 ,350);
         setVisible(true);
     }
 }
--- Book Management System/src/ReturnScreen/RFM.java
+++ Book Management System/src/ReturnScreen/RFM.java
@@ -1,6 +1,7 @@
 package ReturnScreen;
 
 
+import ReturnScreen.BackHomeAction;
 import MainScreen.ReturnAction;
 
 import javax.swing.*;
@@ -17,41 +18,53 @@ public class RFM extends JFrame{
         suf.setLayout(null);
 
 //      ラベル欄
-        JLabel lab ,lan , lalp ,aw , ren , ret;
+        JLabel lan ,lab , lalp ,lapc ,laad ,aw , laren , laret;
+        lan = new JLabel("氏名");
+        lan.setBounds(30,75,80,30);
+        suf.add(lan);
+        lapc =new JLabel("郵便番号");
+        lapc.setBounds(30,120,80,30);
+        suf.add(lapc);
+        laad = new JLabel("住所");
+        laad.setBounds(30, 165, 80, 30);
+        suf.add(laad);
         lab = new JLabel("書籍名");
         lab.setBounds(30, 30, 80, 30);
         suf.add(lab);
-        lan = new JLabel("氏名");
-        lan.setBounds(30, 75, 80, 30);
-        suf.add(lan);
-        ren = new JLabel("貸出日");
-        ren.setBounds(122,110,80,30);
-        suf.add(ren);
-        ret = new JLabel("返却日");
-        ret.setBounds(272,110,80,30);
-        suf.add(ret);
+        laren = new JLabel("貸出日");
+        laren.setBounds(122,195,80,30);
+        suf.add(laren);
+        laret = new JLabel("返却日");
+        laret.setBounds(272,195,80,30);
+        suf.add(laret);
         aw = new JLabel("〜");
-        aw.setBounds(210,135,80,30);
+        aw.setBounds(210,195,80,30);
         suf.add(aw);
         lalp = new JLabel("貸出期間");
-        lalp.setBounds(30,135,80,30);
+        lalp.setBounds(30,220,80,30);
         suf.add(lalp);
 
 //      テキストフォーマット
-        JTextField tfn , tfLs , tfLe;
+        JTextField tfn , tfpc ,tfad , tfLs , tfLe;
         tfn =new JTextField(7);
-        tfn.setBounds(90,75,105,30);
+        tfn.setBounds(90,75, 105,30);
         suf.add(tfn);
+        tfpc = new JTextField(8);
+        tfpc.setBounds(90,120, 80,30);
+        suf.add(tfpc);
+        tfad = new JTextField();
+        tfad.setBounds(90,165,320,30);
+        suf.add(tfad);
         tfLs = new JTextField();
-        tfLs.setBounds(90,135,105,30);
+        tfLs.setBounds(90,220,105,30);
         suf.add(tfLs);
         tfLe = new JTextField();
-        tfLe.setBounds(240,135,105,30);
+        tfLe.setBounds(240,220,105,30);
         suf.add(tfLe);
 
 //      コンボボックス欄
         String[] bl = {"３ステップでしっかり学ぶ PHP入門" , "３ステップでしっかり学ぶ javaScript入門"
-        ,"スラスラわかるHTML&CSSのきほん" ,"ぜったいできます！ ワード＆エクセル超入門",  "ビジネス実務法務試験3級"};
+                ,"スラスラわかるHTML&CSSのきほん" ,"ぜったいできます！ ワード＆エクセル超入門",  "ビジネス実務法務試験3級"};
         JComboBox<String> cbl = new JComboBox<String>(bl);
         cbl.setBounds(90,30,320,30);
         suf.add(cbl);
@@ -59,17 +72,17 @@ public class RFM extends JFrame{
 //      ボタン欄
         JButton bur , bub;
         bur = new JButton("返却");
-        bur.setBounds(240,180,75,35);
+        bur.setBounds(240,265,75,35);
         suf.add(bur);
         bub = new JButton(new BackHomeAction(this));
-        bub.setBounds(325,180,75,35);
+        bub.setBounds(325,265,75,35);
         suf.add(bub);
 
         Container ContentPane = getContentPane();
         ContentPane.add(suf);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(430 ,270);
+        setSize(430 ,350);
         setVisible(true);
     }
 }
